6a3ce1f03683ef57e83f1ea6d4fa35f61af53ca0
Hermann Rasch <hermann.rasch93@gmail.com>
2024-01-08 15:49:15 +0100

    Add signup functionality and fix chat deletion route in api-communicator
    - Implemented `signupUser` function in `api-communicator.ts` to handle user signups. This function makes a POST request to `/user/signup` and throws an error if the response status is not 201 (Created).
    - Corrected the path in the `deleteUserChats` function from `/chat/all-chats` to `/chat/delete`. This change ensures that the function correctly targets the API endpoint for deleting chats.

--- Frontend/src/components/helpers/api-communicator.ts
+++ Frontend/src/components/helpers/api-communicator.ts
@@ -9,6 +9,15 @@ export const loginUser = async (email: string, password: string) => {
     return data;
 };
 
+export const signupUser = async (name: string, email: string, password: string) => {
+    const res = await axios.post("/user/signup" , { name, email, password });
+    if(res.status !== 201) {
+        throw new Error("Unable to signup");
+    }
+    const data = await res.data;
+    return data;
+};
+
 export const checkAuthStatus = async () => {
     const res = await axios.get("/user/auth-status");
     if(res.status !== 200) {
@@ -37,7 +46,7 @@ export const getUserChats = async () => {
 };
 
 export const deleteUserChats = async () => {
-    const res = await axios.delete("/chat/all-chats");
+    const res = await axios.delete("/chat/delete");
     if(res.status !== 200) {
         throw new Error("Unable to delete chat");
     }
