3d216107e0d3ce04b4dac6a02fa38033be867df8
Hermann Rasch <hermann.rasch93@gmail.com>
2024-01-08 13:24:29 +0100

    Redirect to chat page on successful login
    - Imported `useEffect` and `useNavigate` from React and 'react-router-dom' respectively in `login.tsx`.
    - Added a `useEffect` hook to check if the user is already authenticated.
    - Implemented redirection logic within `useEffect` to navigate the user to the '/chat' page if they are already logged in. This ensures that logged-in users are not presented with the login page again.
    - The `useEffect` hook depends on the `auth` object, ensuring it only runs when there is a change in the user's authentication status.

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Typography, Button } from "@mui/material";
 import { IoLogInOutline } from "react-icons/io5";
 import CustomizedInput from '../components/shared/Customizedinput';
 import { useAuth } from '../context/AuthContext';
-import { toast } from "react-hot-toast"
+import { toast } from "react-hot-toast";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
@@ -21,6 +23,11 @@ const Login = () => {
       toast.error("Sign In Failed", { id: "login" })
     }
   }
+  useEffect(() => {
+    if(auth?.user) {
+      return navigate("/chat");
+    }
+  }, [auth])
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md:"flex", sm:"none", xs:"none" }}>
