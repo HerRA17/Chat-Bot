29306c2700152440f4d59883cf9adcf6f4e65f94
Hermann Rasch <hermann.rasch93@gmail.com>
2024-01-14 21:07:55 +0100

    Update AuthContext.tsx

--- Frontend/src/context/AuthContext.tsx
+++ Frontend/src/context/AuthContext.tsx
@@ -34,14 +34,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const login = async (email: string, password: string) => {
         const data = await loginUser(email, password);
         if (data) {
-            setUser({email: data.email, name: data.name})
+            setUser({ email: data.email, name: data.name })
             setIsLoggedIn(true);
         } 
     };
     const signup = async (name: string, email: string, password: string) => {
         const data = await signupUser(name, email, password);
         if (data) {
-            setUser({email: data.email, name: data.name})
+            setUser({ email: data.email, name: data.name })
             setIsLoggedIn(true);
         } 
     };
